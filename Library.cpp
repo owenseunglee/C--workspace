89a972c9dc61f7c6ada964946b10c6d7d71cd813
Owen Lee <owenseunglee@gmail.com>
2023-10-02 09:54:14 -0700

    added comments

--- Library.cpp
+++ Library.cpp
@@ -1,111 +1,36 @@
-#include <iostream>
+// ============================================================================
+// File: Library.cpp
+// ============================================================================
+// Programmger: Owen Lee
+// Project: Library Simulator
+// 
+// Description: This program is a library simulator that involves multiple
+//              classes and it is aimed to implement patron interactions
+//              and transactional operations using encapsulation, inheritance,
+//              and polymorphism.
+// 
+// ============================================================================
+#include "Library.hpp"
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-string const none = "None";
-string const hold = "ON_HOLD";
-string const out = "CHEKCED_OUT";
-string const in = "ON_SHELF";
-
-class LibraryItem
+void Patron::remove_library_item(LibraryItem* return_item)
 {
-    public:
-    LibraryItem(int id, string title){
-        m_library_item_id = id;
-        m_title = title;
-        m_requested_by = none;
-        m_checked_out_by = none;
-        m_location = in;};
-    string get_location(){return m_location;};
-    int get_library_id(){return m_library_item_id;};
-    string get_title(){return m_title;};
-    string get_requested_by(){return m_requested_by;};
-    void set_checked_out_by(int patron){m_checked_out_by = patron;};
-    int get_checked_out_by(){return m_checked_out_by;};
-    void set_requested_by(string patron){m_requested_by = patron;};
-    void set_date_checked_out(int date){m_date_checked_out = date;};
-    int get_date_checked_out(){return m_date_checked_out;};
-    void set_location(string new_location){m_location = new_location;};
-    virtual int get_check_out_length(){return 0;};
-
-    private:
-    int m_library_item_id;
-    string m_title;
-    string m_location;
-    int m_checked_out_by;
-    string m_requested_by;
-    int m_date_checked_out;
-};
-
-class Book : public LibraryItem{
-    public:
-    static const int check_out_length = 21;
-    Book(int id, string title, string author) : LibraryItem(id, title), m_author(author){};
-    string get_author(){return m_author;};
-    int get_check_out_length() override {return check_out_length;};
-    private:
-    string m_author;
-};
-
-class Album : public LibraryItem{
-    public:
-    static const int check_out_length = 14;
-    Album(int id, string title, string artist) : LibraryItem(id, title), m_artist(artist){};
-    string get_artist(){return m_artists;};
-    int get_check_out_length() override {return check_out_length};
-    private:
-    string m_artists;
-};
-
-class Movie : public LibraryItem{
-    public:
-    static const int check_out_length = 7;
-    Movie(int id, string title, string director) : LibraryItem(id, title), m_director(director){};
-    string get_director(){return m_director;};
-    int get_check_out_length() override {return check_out_length};
-};
-
-class Patron{
-    public:
-    Patron(int id, string name){
-        m_patron_id = id;
-        m_name = name;
-        m_fine_amount = 0.0;};
-    double get_fine_amount(){return m_fine_amount;};
-    void add_library_item(LibraryItem* add_item){
-        checked_out_items.push_back(add_item);
-    };
-    void remove_library_item(LibraryItem* return_item){
-        for (vector<LibraryItem*>::iterator it = checked_out_items.begin(); it != checked_out_items.end(); ++it)
+    for (vector<LibraryItem*>::iterator it = checked_out_items.begin(); it !
+         = checked_out_items.end(); ++it)
+    {
+        if (*it == return_item)
         {
-            if(*it == return_item)
-            {
-                checked_out_items.erase(it);
-                break;
-            }
-        };};
-    string get_patron_name(){return m_name;};
-    int get_patron_id(){return m_patron_id;};
-    vector<LibraryItem*> get_checked_out_items(){return checked_out_items;};
-    void amend_fine(double amount){m_fine_amount += amount;};
-    
-    private:
-    int m_patron_id;
-    string m_name;
-    double m_fine_amount;
-    vector<LibraryItem*> checked_out_items;
-};
+            checked_out_items.erase(it);
+            break;
+        }
+    };
+}; // end of remove_library_item
 
-class Library
+LibraryItem* Library::lookup_library_item_from_id(int item_id)
 {
-    public:
-    Library(){
-        m_current_date = 0;};
-    void add_library_item(LibraryItem* item){holdings.push_back(item);};
-    void add_patron(Patron* patron){members.push_back(patron);};
-    LibraryItem* lookup_library_item_from_id(int item_id){
         for (LibraryItem*& item : holdings)
         {
             if (item -> get_library_id() == item_id)
@@ -114,142 +39,132 @@ class Library
             }
         }
         return nullptr;
-    };
-    Patron* lookup_patron_from_id(int patron_id)
+}; // end of lookup_library_item_from_id
+
+Patron* Library::lookup_patron_from_id(int patron_id)
+{
+    for (Patron*& item : members)
+    
+        if (patron -> get_patron_id() == patron_id)
+            return patron;
+
+    return nullptr;
+}; // end of lookup_patron_from_id
+
+string Library::check_out_library_item(int patron_id, int item_id)
+{
+    Patron* patron = lookup_patron_from_id(patron_id);
+
+    if (!patron)
+        return "Patron not found";
+
+    LibraryItem* item = lookup_library_item_from_id(item_id);
+
+    if (!item)
+        return "Item not found";
+
+    if (item->get_location() == OUT)
+        return "Item already checked out";
+
+    else if (item->get_location() == IN)
     {
-        for (Patron*& item : members)
+        if (item->get_requested_by() == patron -> get_patron_name() 
+            || item -> get_requested_by() == NONE)
         {
-            if (patron -> get_patron_id() == patron_id)
-            {
-                return patron;
-            }
-        }
-        return nullptr;
-    };
-    string check_out_library_item(int patron_id, int item_id)
-    {
-        Patron* patron = lookup_patron_from_id(patron_id);
-
-        if (!patron)
-        {
-            return "Patron not found";
-        }
-
-        LibraryItem* item = lookup_library_item_from_id(item_id);
-
-        if (!item)
-        {
-            return "Item not found";
-        }
-
-        if(item->get_location() == out)
-        {
-            return "Item already checked out";
-        }
-        else if(item->get_location() == in)
-        {
-            if(item->get_requested_by() == patron -> get_patron_name() || item -> get_requested_by() == none)
-            {
-                item -> set_requested_by(none);
-                item -> set_checked_out_by(patron_id);
-                item -> set_location(out);
-                item -> set_date_checked_out(m_current_date);
-                patron -> add_library_item(item);
-            }
-            else
-            {
-                return "Item on hold by other patron";
-            }
-            return "Check out successful";
-        }
-    }
-
-    string return_library_item(int item_id)
-    {
-        LibraryItem* item = lookup_library_item_from_id(item_id);
-
-        if(!item)
-            return "Item not found";
-
-        if (item -> get_location() == out)
-        {
-            if (item -> get_requested_by() != none)
-            {
-                item -> set_location(hold);
-            }
-            else
-            {
-                item -> set_location(in);
-            }
-            Patron* patron = lookup_patron_from_id(item -> get_checked_out_by());
-            patron -> remove_library_item(item);
-            return "Return successful";
+            item -> set_requested_by(NONE);
+            item -> set_checked_out_by(patron_id);
+            item -> set_location(OUT);
+            item -> set_date_checked_out(m_current_date);
+            patron -> add_library_item(item);
         }
         else
-            return "Item already in library";
+            return "Item on hold by other patron";
+        
+        return "Check out successful";
     }
+} // end of check_out_library_item
 
-    string request_library_item(int patron_id, int item_id)
+string Library::return_library_item(int item_id)
+{
+    LibraryItem* item = lookup_library_item_from_id(item_id);
+
+    if (!item)
+        return "Item not found";
+
+    if (item -> get_location() == OUT)
     {
-        LibraryItem* item = lookup_library_item_from_id(item_id);
-        Patron* patron = lookup_patron_from_id(patron_id);
-
-        if(!item)
-            return "Item not found";
-        else if(!patron)
-            return "Patron not found";
-
-        if(item->get_requested_by() != none)
+        if (item -> get_requested_by() != NONE)
         {
-            return "Item already on hold";
+            item -> set_location(HOLD);
         }
         else
         {
-            item -> set_location(hold);
-            item -> set_requested_by(patron -> get_patron_name());
+            item -> set_location(IN);
         }
-        return "Request Successful";
+        Patron* patron = lookup_patron_from_id(item -> get_checked_out_by());
+        patron -> remove_library_item(item);
+        
+        return "Return successful";
     }
+    else
+        return "Item already in library";
+} // end of return_library_item
 
-    string pay_fine(int patron_id, double amount)
+string Library::request_library_item(int patron_id, int item_id)
+{
+    LibraryItem* item = lookup_library_item_from_id(item_id);
+    Patron* patron = lookup_patron_from_id(patron_id);
+
+    if (!item)
+        return "Item not found";
+    else if (!patron)
+        return "Patron not found";
+
+    if (item->get_requested_by() != NONE)
     {
-        Patron* patron = lookup_patron_from_id(patron_id);
-
-        if(!patron)
-            return "Patron not found";
-        else
-            patron -> amend_fine(-amount);
-
-        return "Payment Successful";
+        return "Item already on hold";
     }
-
-    void increment_current_date()
+    else
     {
-        double fine = .10;
-        ++m_current_date;
-        for (Patron* patron : members)
+        item -> set_location(HOLD);
+        item -> set_requested_by(patron -> get_patron_name());
+    }
+    return "Request Successful";
+} // end of request_library_item
+
+string Library::pay_fine(int patron_id, double amount)
+{
+    Patron* patron = lookup_patron_from_id(patron_id);
+
+    if(!patron)
+        return "Patron not found";
+    else
+        patron -> amend_fine(-amount);
+
+    return "Payment Successful";
+} // end of pay_fine
+
+void Library::increment_current_date()
+{
+    double fine = .10;
+    ++m_current_date;
+
+    for (Patron* patron : members)
+    {
+        vector<LibraryItem*> checked_out = patron -> get_checked_out_items();
+
+        for (LibraryItem* items : checked_out)
         {
-            vector<LibraryItem*> checked_out = patron -> get_checked_out_items();
+            int days_checked_out = m_current_date - 
+                                   (items -> get_date_checked_out());
+            int overdue = days_checked_out - (items -> get_check_out_length());
 
-            for (LibraryItem* items : checked_out)
-            {
-                int days_checked_out = m_current_date - (items -> get_date_checked_out());
-                int overdue = days_checked_out - (items -> get_check_out_length());
-
-                if(overdue > 0)
-                {
-                    patron -> amend_fine(fine);
-                }
-            }
+            if (overdue > 0)
+                patron -> amend_fine(fine);
         }
     }
-
-    private:
-    vector<LibraryItem*> holdings;
-    vector<Patron*> members;
-    int m_current_date;
-};
-
+} // end of increment_current_date
+        
 int main()
 {
     Book b(123, "Phantom", "Juster");
--- Library.hpp
+++ Library.hpp
@@ -1,22 +1,34 @@
+// ============================================================================
+// File: Library.hpp
+// ============================================================================
+// Programmger: Owen Lee
+// Project: Library Simulator
+// 
+// Description: This is a header file for Library.cpp
+// 
+// ============================================================================
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-string const none = "None";
-string const hold = "ON_HOLD";
-string const out = "CHEKCED_OUT";
-string const in = "ON_SHELF";
+// global variables for the location of the items
+string const NONE = "None";
+string const HOLD = "ON_HOLD";
+string const OUT = "CHEKCED_OUT";
+string const IN = "ON_SHELF";
 
+// LibraryItem class declaration 
 class LibraryItem
 {
-    public:
+public:
     LibraryItem(int id, string title){
         m_library_item_id = id;
         m_title = title;
-        m_requested_by = none;
-        m_checked_out_by = none;
-        m_location = in;};
+        m_requested_by = NONE;
+        m_checked_out_by = NONE;
+        m_location = IN;
+    };
     string get_location(){return m_location;};
     int get_library_id(){return m_library_item_id;};
     string get_title(){return m_title;};
@@ -29,7 +41,7 @@ class LibraryItem
     void set_location(string new_location){m_location = new_location;};
     virtual int get_check_out_length(){return 0;};
 
-    private:
+private:
     int m_library_item_id;
     string m_title;
     string m_location;
@@ -38,62 +50,85 @@ class LibraryItem
     int m_date_checked_out;
 };
 
+// Book class declaration
 class Book : public LibraryItem{
-    public:
+public:
     static const int check_out_length = 21;
-    Book(int id, string title, string author) : LibraryItem(id, title), m_author(author){};
+    Book(int id, string title, string author) : 
+            LibraryItem(id, title), m_author(author){};
     string get_author(){return m_author;};
     int get_check_out_length() override {return check_out_length;};
-    private:
+    
+private:
     string m_author;
 };
 
+// Album class declaration
 class Album : public LibraryItem{
-    public:
+public:
     static const int check_out_length = 14;
-    Album(int id, string title, string artist) : LibraryItem(id, title), m_artist(artist){};
-    string get_artist(){return m_artists;};
+    Album(int id, string title, string artist) : 
+            LibraryItem(id, title), m_artist(artist){};
+    string get_artist(){return m_artist;};
     int get_check_out_length() override {return check_out_length};
-    private:
-    string m_artists;
+    
+private:
+    string m_artist;
 };
 
+// Movie class declaration
 class Movie : public LibraryItem{
-    public:
+public:
     static const int check_out_length = 7;
-    Movie(int id, string title, string director) : LibraryItem(id, title), m_director(director){};
+    Movie(int id, string title, string director) : 
+            LibraryItem(id, title), m_director(director){};
     string get_director(){return m_director;};
     int get_check_out_length() override {return check_out_length};
+
+private:
+    string m_director;
 };
 
+// Patron class declaration
 class Patron{
-    public:
+public:
     Patron(int id, string name){
         m_patron_id = id;
         m_name = name;
         m_fine_amount = 0.0;};
     double get_fine_amount(){return m_fine_amount;};
     void add_library_item(LibraryItem* add_item){
-        checked_out_items.push_back(add_item);
-    };
-    void remove_library_item(LibraryItem* return_item){
-        for (vector<LibraryItem*>::iterator it = checked_out_items.begin(); it != checked_out_items.end(); ++it)
-        {
-            if(*it == return_item)
-            {
-                checked_out_items.erase(it);
-                break;
-            }
-        };};
+        checked_out_items.push_back(add_item);};
+    void remove_library_item(LibraryItem* return_item)
     string get_patron_name(){return m_name;};
     int get_patron_id(){return m_patron_id;};
     vector<LibraryItem*> get_checked_out_items(){return checked_out_items;};
     void amend_fine(double amount){m_fine_amount += amount;};
     
-    private:
+private:
     int m_patron_id;
     string m_name;
     double m_fine_amount;
     vector<LibraryItem*> checked_out_items;
 };
 
+// Library class declaration
+class Library
+{
+public:
+    Library(){
+        m_current_date = 0;};
+    void add_library_item(LibraryItem* item){holdings.push_back(item);};
+    void add_patron(Patron* patron){members.push_back(patron);};
+    LibraryItem* lookup_library_item_from_id(int item_id)
+    Patron* lookup_patron_from_id(int patron_id)
+    string return_library_item(int item_id)
+    string request_library_item(int patron_id, int item_id)
+    string pay_fine(int patron_id, double amount)
+    void increment_current_date()
+    
+private:
+    vector<LibraryItem*> holdings;
+    vector<Patron*> members;
+    int m_current_date;
+};
